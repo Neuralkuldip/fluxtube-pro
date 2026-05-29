9124598cac2b4a5fa7963095c39109c4eac6c63c
Neuralkuldip <sutharkuldip116@gmail.com>
2026-05-29 15:49:48 +0530

    Switch to Railway backend

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
 
-  baseURL: "fluxtube-pro-production.up.railway.app",
+  baseURL: "https://fluxtube-pro-production.up.railway.app",
 });
 
 export default api;
